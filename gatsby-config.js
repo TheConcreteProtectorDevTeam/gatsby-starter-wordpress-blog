243978c1c6c3e38d147aeffcc04103e39193d0bd
The Concrete Protector <TheConcreteProtectorDevTeam@gmail.com>
2021-04-12 16:26:11 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://m.theconcreteprotector.com/graphql`,
+          `m.theconcreteprotector.com/graphql`,
       },
     },
 
